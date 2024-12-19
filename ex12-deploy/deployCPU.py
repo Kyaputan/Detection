c8fc53bb1184bdfc0f10b9af7c5f7c680263bfcd
Rachata Singkhet <singkhet1@gmail.com>
2024-12-19 22:02:44 +0700

    add Deploy

---
+++ ex12-deploy/deployCPU.py
@@ -0,0 +1,9 @@
+from ultralytics import YOLO
+import os
+
+folder_path = os.path.dirname(os.path.realpath(__file__))
+model_path = os.path.join(folder_path, "model.pt")
+model = YOLO(model_path) 
+
+model.export(format='onnx',simplify=True,int8=True,imgsz=640)       
+
