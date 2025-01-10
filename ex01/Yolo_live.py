ec74fcd15579d687fe7870869bd4673918d629df
Rachata Singkhet <singkhet1@gmail.com>
2025-01-10 14:30:50 +0700

    Add Track

--- ex01/Yolo_live.py
+++ ex01/Yolo_live.py
@@ -2,11 +2,11 @@ import cv2
 import os
 from ultralytics import YOLO
 
-folder_path = os.path.dirname(os.path.realpath(__file__))
+# folder_path = os.path.dirname(os.path.realpath(__file__))
 # model_path = os.path.join(folder_path, "yolo11n.onnx")
-model_path = os.path.join(folder_path, "modelYolo.onnx")
-model = YOLO(model_path , task='detect')
-# model = YOLO("yolo11n.pt")
+# model_path = os.path.join(folder_path, "modelYolo.onnx")
+# model = YOLO(model_path , task='detect')
+model = YOLO("yolo11n-pose.onnx")
 
 cap = cv2.VideoCapture(0)
 
@@ -14,7 +14,8 @@ while cap.isOpened():
     success, frame = cap.read()
 
     if success:
-        results = model(frame)
+        # results = model(frame)
+        results = model.track(frame, persist=True)
         annotated_frame = results[0].plot()
         cv2.imshow("YOLO Inference", annotated_frame)
         if cv2.waitKey(1) & 0xFF == ord("q"):
--- ex12-deploy/deployCPU.py
+++ ex12-deploy/deployCPU.py
@@ -4,6 +4,6 @@ import os
 # folder_path = os.path.dirname(os.path.realpath(__file__))
 # model_path = os.path.join(folder_path, "model.pt")
 # model = YOLO(model_path)
-model = YOLO('yolo11n.pt')
+model = YOLO('yolo11n-pose.pt')
 model.export(format='onnx',simplify=True,int8=True,imgsz=640) #ONNX for CPU , TensorRT for GPU 
 
