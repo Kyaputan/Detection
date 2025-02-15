76030707880dd563f52f99d598f2e2d0d32a47b4
Rachata Singkhet <singkhet1@gmail.com>
2025-02-16 01:09:08 +0700

    Update heat.py

--- ex01/heat.py
+++ ex01/heat.py
@@ -9,11 +9,13 @@ folder_path = os.path.dirname(os.path.realpath(__file__))
 model_path = os.path.join(folder_path, "best.pt")
 video_path = os.path.join(folder_path, "car_top2.mp4")
 model = YOLO(model_path)
+
 region_points = [(460, 540), (500, 540), (500, 0), (460, 0)]
+
 # region_points = [(480, 0), (480, 960)]
 cap = cv2.VideoCapture(video_path)
 
-video_writer = cv2.VideoWriter('heatmap.mp4', -1, 35.0, (960,540))
+# video_writer = cv2.VideoWriter('heatmap.mp4', -1, 35.0, (960,540))
 track_history = defaultdict(lambda: [])
 
 
@@ -44,14 +46,14 @@ while cap.isOpened():
             
             track = track_history[track_id]
             track.append((float(x), float(y)))  # x, y center point
-            if len(track) > 30:  # retain 90 tracks for 90 frames
+            if len(track) > 30:
                 track.pop(0)
 
             points = np.hstack(track).astype(np.int32).reshape((-1, 1, 2))
             cv2.polylines(annotated_frame, [points], isClosed=False, color=(230, 230, 230), thickness=2)
         
         
-        video_writer.write(annotated_frame)
+        # video_writer.write(annotated_frame)
         cv2.imshow("YOLO11 Tracking", annotated_frame)
         
         
@@ -62,5 +64,5 @@ while cap.isOpened():
         break
 
 cap.release()
-video_writer.release()
+# video_writer.release()
 cv2.destroyAllWindows()
