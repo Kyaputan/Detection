304cd92c6c70e2bf2fc9e88d71830f181fe8c7d6
Rachata Singkhet <161452629+Kyaputan@users.noreply.github.com>
2025-08-20 08:30:09 +0700

    ...

--- ex12-deploy/deployCPU.py
+++ ex12-deploy/deployCPU.py
@@ -1,13 +1,9 @@
-from ultralytics import YOLO , RTDETR
+from ultralytics import YOLO
 import os
 
 folder_path = os.path.dirname(os.path.realpath(__file__))
-model_path = os.path.join(folder_path, "ModelYolo.pt")
+model_path = os.path.join(folder_path, "guard.pt")
 
 model = YOLO(model_path)
 
 model.export(format="onnx", simplify=True, imgsz=640, dynamic=False, nms=True)
-
-
-
-
